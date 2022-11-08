4caee7e791df1effd5f671bc27ed8708c4293f17
thuanpham113 <thuanpham113@users.noreply.github.com>
2022-11-08 04:31:28 +0000

    deploy: 84761d9c7be55512c8ab9a88cd5aa9e6f92564a8

--- _nuxt/static/1667854143/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Ffr\u002Fcontact","\u002Fcontact","\u002Flogin","\u002Ffr\u002Flogin","\u002Ffr","\u002Ffr\u002Fwork","\u002Fwork"]})
---
+++ _nuxt/static/1667881879/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Ffr\u002Fcontact","\u002Fcontact","\u002Flogin","\u002Ffr\u002Flogin","\u002Ffr","\u002Fwork","\u002Ffr\u002Fwork"]})
